29155989e53a790b89b7b95f42bfb0b60d7382ff
tory-mo <viktoria.moz@gmail.com>
2018-05-04 10:35:23 +0200

    animation and checking added

--- app/src/main/java/by/torymo/navigationdrawer/MainActivity.java
+++ app/src/main/java/by/torymo/navigationdrawer/MainActivity.java
@@ -1,11 +1,15 @@
 package by.torymo.navigationdrawer;
 
+
+
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +18,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
 
-
-
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
+
+        final NavigationMenuItem llBasic = navigationView.findViewById(R.id.nav_basic);
+        final NavigationMenuItem llDriver = navigationView.findViewById(R.id.nav_driver);
+        final NavigationMenuItem llDach = navigationView.findViewById(R.id.nav_country);
+        final NavigationMenuItem llFisher = navigationView.findViewById(R.id.nav_fisher);
+        final NavigationMenuItem llTourist = navigationView.findViewById(R.id.nav_tourist);
+        final NavigationMenuItem llEcologist = navigationView.findViewById(R.id.nav_ecologist);
+        final NavigationMenuItem llSettings = navigationView.findViewById(R.id.nav_settings);
+        final NavigationMenuItem llWarning = navigationView.findViewById(R.id.nav_warning);
+
+        View.OnClickListener navClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                String chosenProfile = "";
+
+                llBasic.setChecked(false);
+                llDriver.setChecked(false);
+                llDach.setChecked(false);
+                llFisher.setChecked(false);
+                llTourist.setChecked(false);
+                llEcologist.setChecked(false);
+                llSettings.setChecked(false);
+                llWarning.setChecked(false);
+                NavigationMenuItem navigationMenuItem = (NavigationMenuItem) view;
+                navigationMenuItem.setChecked(true);
+
+                switch (id) {
+                    case R.id.nav_basic:
+                        chosenProfile = getString(R.string.profile_base);
+                        break;
+                    case R.id.nav_country:
+                        chosenProfile = getString(R.string.profile_dach);
+                        break;
+                    case R.id.nav_driver:
+                        chosenProfile = getString(R.string.profile_auto);
+                        break;
+                    case R.id.nav_ecologist:
+                        chosenProfile = getString(R.string.profile_ecologyst);
+
+                        break;
+                    case R.id.nav_fisher:
+                        chosenProfile = getString(R.string.profile_fisherman);
+                        break;
+                    case R.id.nav_tourist:
+                        chosenProfile = getString(R.string.profile_tourist);
+                        break;
+                    case R.id.nav_settings:
+//                        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+//                        startActivity(intent);
+                        drawer.closeDrawers();
+                        return;
+                    case R.id.nav_warning:
+//                        Intent warningIntent = new Intent(MainActivity.this, StormActivity.class);
+//                        warningIntent.putExtra(StormActivity.ACTIVITY_EXTRA, NUMBER);
+//                        startActivityForResult(warningIntent, STORM_REQUEST_CODE);
+                        drawer.closeDrawers();
+                        return;
+                }
+                //selectProfile(chosenProfile, true);
+                Toast.makeText(MainActivity.this,id + " - " + chosenProfile,Toast.LENGTH_LONG).show();
+                drawer.closeDrawers();
+                return;
+            }
+        };
+
+        llBasic.setOnClickListener(navClick);
+        llDriver.setOnClickListener(navClick);
+        llDach.setOnClickListener(navClick);
+        llFisher.setOnClickListener(navClick);
+        llTourist.setOnClickListener(navClick);
+        llEcologist.setOnClickListener(navClick);
+        llSettings.setOnClickListener(navClick);
+        llWarning.setOnClickListener(navClick);
+
     }
 }
---
+++ app/src/main/java/by/torymo/navigationdrawer/NavigationMenuItem.java
@@ -0,0 +1,126 @@
+package by.torymo.navigationdrawer;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class NavigationMenuItem extends LinearLayout {
+
+    private ImageView icon;
+    private TextView text;
+
+    private boolean checkable = false;
+
+    private int iconId;
+    private int iconTint;
+    private int checkedColor;
+    private int pressedColor;
+    private ColorStateList textColor;
+    private String sText;
+
+    private boolean isCreated;
+    private Context context;
+
+    public NavigationMenuItem(Context context) {
+        super(context);
+        init(context,null, 0);
+    }
+
+    public NavigationMenuItem(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context,attrs, 0);
+    }
+
+    public NavigationMenuItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context,attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public NavigationMenuItem(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context,attrs, defStyleAttr);
+    }
+
+    private void init(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+
+        this.context = context;
+
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(inflater == null) return;
+
+        inflater.inflate(R.layout.navigation_menu_item, this);
+
+        icon = findViewById(R.id.ivIcon);
+        text = findViewById(R.id.tvText);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.NavigationMenuItem, defStyleAttr, 0);
+
+        iconId = a.getResourceId(R.styleable.NavigationMenuItem_navItemIcon, 0);
+        iconTint = a.getColor(R.styleable.NavigationMenuItem_navItemIconTint, 0);
+        checkedColor = a.getColor(R.styleable.NavigationMenuItem_navCheckedItemTextColor, 0);
+        pressedColor = a.getColor(R.styleable.NavigationMenuItem_navItemPressedColor, getResources().getColor(R.color.pressed_color));
+        sText = a.getString(R.styleable.NavigationMenuItem_navItemText);
+        checkable = a.getBoolean(R.styleable.NavigationMenuItem_navItemCheckable, checkable);
+
+//        TypedValue iconTintValue = new TypedValue();
+//        a.getValue(R.styleable.NavigationMenuItem_navItemIconTint, iconTintValue);
+//        iconTint  = 0;
+//        if (iconTintValue.type == TypedValue.TYPE_REFERENCE) {
+//            iconTint = iconTintValue.resourceId;
+//        } else {
+//            iconTint = iconTintValue.data;
+//        }
+
+        a.recycle();
+
+        textColor = text.getTextColors();
+    }
+
+
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (!isCreated) {
+            buildView();
+            isCreated = true;
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (!isCreated) {
+            buildView();
+            isCreated = true;
+        }
+    }
+
+    private void buildView(){
+        text.setText(sText);
+
+        icon.setImageResource(iconId);
+        icon.setColorFilter(iconTint, android.graphics.PorterDuff.Mode.SRC_IN);
+    }
+
+    public void setChecked(boolean checked){
+        if(!checkable || text == null) return;
+        if(checked){
+            text.setTextColor(checkedColor);
+
+        }else {
+            text.setTextColor(textColor);
+        }
+    }
+
+}
